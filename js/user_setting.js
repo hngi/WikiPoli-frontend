f3fbd9b0cb81763e1e1c7317a2d90307479c3f0c
JamesChima <chima.mjames@hotmail.com>
2019-10-24 17:39:45 +0100

    deleted some files

--- js/user_setting.js
+++
@@ -1,9 +0,0 @@
-function toggleOn(obj) {
-	obj.style.display="none";
-	obj.nextElementSibling.style.display = "inline";
-}
-
-function toggleOff(obj) {
-	obj.style.display= "none";
-	obj.previousElementSibling.style.display ="inline";
-}
