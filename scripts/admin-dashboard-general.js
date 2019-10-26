ff095ba050f91a9150f6c1cf4685472a3dc82536
Johnson Ubaezuonu <jaycodist@gmail.com>
2019-10-26 03:14:11 +0100

    reused sidebar in activity dashboard -Johnson

--- scripts/admin-dashboard-general.js
+++ scripts/admin-dashboard-general.js
@@ -15,41 +15,4 @@ if ( !wikiToken && !userEmail ) {
       location.href="signin.html"
     }, 500);
   } )  
-  document.querySelector('#add-post-button').addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log( 'show-modal' )
-    const addPosts = () => {
-      const postBody = document.querySelector("#post-content").value
-
-      var form = new FormData();
-      form.append(
-        "token",
-        localStorage.getItem('wikiToken')
-      );
-      form.append("post", postBody);
-      form.append("topic", "tester");
-
-      var settings = {
-        url: `${api}/create_post.php`,
-        method: "POST",
-        timeout: 0,
-        processData: false,
-        mimeType: "multipart/form-data",
-        contentType: false,
-        data: form
-      };
-
-      $.ajax(settings).done(function(response) {
-        console.log('entered');
-        console.log(JSON.parse(response));
-      });
-    }
-    addPosts();
-    console.log('done');
-    setTimeout(() => {
-      document.location.reload()
-    }, 1000);
-
-  })
-
 }
